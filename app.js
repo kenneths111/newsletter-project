05e0bfcb3d70a6b329590bfaccb3d695868c9e1e
Kenneth Sng <kenneth@open.gov.sg>
2023-07-12 00:57:22 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_field: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/38bdddfd15";
+  const options = {
+    method: "POST",
+    auth: "kennethsng:57c659bee3ebed1ca8d6606174ec98c1-us21",
+  };
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      //console.log(JSON.parse(data));
+      console.log(response.statusCode);
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/failure.html");
+      } else {
+        res.send(__dirname + "/failure.html");
+      }
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on Port 3000.");
+});
+
+//API Key: 57c659bee3ebed1ca8d6606174ec98c1-us21
+
+//Audience ID: 38bdddfd15
